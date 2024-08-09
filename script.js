ec204632568df5997ed043949a5b37b5ae2ebfcf
Fodorcea_Dumitru <fodorcea.dumitru2@gmail.com>
2024-08-09 11:01:58 +0300

    refactor(html, css, js) adding a new square 2 class and adding style

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const patrat = document.querySelector('.patrat')
+const patrat2 = document.querySelector('.patrat2')
 
 patrat.addEventListener('click',(ev)=>{
     patrat.classList.toggle('switch')
 })
 
+patrat2.addEventListener('click', (ev) =>{
+    patrat2.classList.toggle('switch2')
+})
